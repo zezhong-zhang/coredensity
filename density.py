125ef5467fa9a193f604bf3c3cbe9ba086a1eaaf
Zezhong <jack.zezhong.zhang@gmail.com>
2024-06-14 16:05:59 +0200

    minor

--- density.py
+++ density.py
@@ -3,7 +3,7 @@ import h5py as h5
 import numpy as np
 import matplotlib.pyplot as plt
 
-with h5.File('orbital/Au.h5', 'r') as f:
+with h5.File('database/Au.h5', 'r') as f:
     indices = list(f.keys())
     indices.sort(key=lambda x: int(x))
     for index in indices:
